e78feea743bc4521cc13387fbd49e0ce6c28bd7d
Sim <me@zhongyuen.dev>
2021-08-13 23:10:40 +0800

    Remove Animated Background

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,17 +1,17 @@
 // import registerServiceWorker from './registerServiceWorker.js'
 import { $ } from './utils/buildComponents'
-import MyComponent from './components/MyComponent/'
+import MainComponent from './components/MainComponent/'
 
 const backgroundImages = ['ammy-static', 'bead']
 
-MyComponent()
+MainComponent()
 
 // @ts-ignore
 $('body').style.backgroundImage = `url(assets/${
   backgroundImages[~~(Math.random() * backgroundImages.length)]
 }.png)`
 
-$('main').appendChild(document.createElement('my-component'))
+$('main').appendChild(document.createElement('main-component'))
 $('#stop-animation').addEventListener('click', _ => {
   $('body').classList.toggle('no-anim')
 })
